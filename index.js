a8bf6b595bb75f85e0061b48a069db29b46ecdd6
Jennifer Hong <hongjenn24@gmail.com>
2021-11-29 00:43:25 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -3,33 +3,50 @@ const dodger = document.getElementById("dodger");
 
 dodger.style.backgroundColor = "#FF69B4"; 
 
-function moveDodgerLeft(){
-    const leftNumbers = dodger.style.left.replace("px","");
-    const left = parseInt(leftNumbers,10);
-
-    if (left > 0){
-        dodger.style.left = `${left -10}px`;
-    }
-
-}
 
 document.addEventListener("keydown", function(e) {
-    if (e.key === "ArrowLeft") {
-      moveDodgerLeft();
-    }
-  }); 
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+}); 
 
-  document.addEventListener("keydown",function(e){
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px","");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0){
+        dodger.style.left = `${left - 10}px`;
+    }
+}
+
+
+  document.addEventListener("keydown", function(e){
     if (e.key === "ArrowRight") {
         moveDodgerRight();
     }
-  })
-
+  });
+  
   function moveDodgerRight(){
     const leftNumbers = dodger.style.left.replace("px","");
-    const left = parseInt(leftNumbers,10);
+    const left = parseInt(leftNumbers, 10);
 
     if (left < 360) {
         dodger.style.left = `${left + 10}px`;
     }
 }
+
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowUp") {
+    moveDodgerUp();
+  }
+}); 
+
+// function moveDodgerUp(){
+//     const upNumbers = dodger.style.up.replace("px","");
+//     const up = parseInt(upNumbers, 10);
+
+//     if (up > 0){
+//         dodger.style.up = `${up - 10}px`;
+//     }
+// }
